f4d7884db17663fec42e35fe2e554823555d407b
John <johndaligault@gmail.com>
2019-05-17 16:19:03 +0200

    fixing mouse on click

--- src/main.go
+++ src/main.go
@@ -6,51 +6,33 @@ import (
 	"syscall/js"
 )
 
-// creating dom as global variable for simplicity
-var dom browser.DOM
-
 func main() {
 	emptyChannel := make(chan bool)
 
 	// loading DOM to memory
-	dom = browser.LoadDOM()
+	DOM := browser.LoadDOM()
 
 	// setting up everything
-	setup()
+	setup(DOM)
 
 	// attempt to receive from empty channel
 	// allow daemon style process
 	<-emptyChannel
 }
 
-func setup() {
-	// javascript rendering func
-	var renderer js.Func
-
-	renderer = js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-		update()
-		return nil
-	})
-
-	// using browser animation frame
-	dom.Window.Call("requestAnimationFrame", renderer)
-
+func setup(DOM browser.DOM) {
 	// let's handle that mouse pointer down
 	var mouseEventHandler = js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-		clickEvent(args[0])
+		clickEvent(DOM, args[0])
 		return nil
 	})
 
-	dom.Window.Call("addEventListener", "pointerdown", mouseEventHandler)
+	DOM.Window.Call("addEventListener", "pointerdown", mouseEventHandler)
 }
 
-func update() {
-	fmt.Printf("updating frame")
-}
-
-func clickEvent(event js.Value) {
+func clickEvent(DOM browser.DOM, event js.Value) {
 	mouseX := event.Get("clientX").Int()
 	mouseY := event.Get("clientY").Int()
 
-	go dom.Log(fmt.Sprintf("mouseEvent x:%d y:%d", mouseX, mouseY))
+	go DOM.Log(fmt.Sprintf("mouseEvent x:%d y:%d", mouseX, mouseY))
 }
