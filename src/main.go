10eb21ed85e1ffa315d89843b56cfba483680dab
John <johndaligault@gmail.com>
2019-05-17 16:08:11 +0200

    now handling mouse click events

--- src/main.go
+++ src/main.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"./browser"
+	"fmt"
+	"syscall/js"
+)
+
+// creating dom as global variable for simplicity
+var dom browser.DOM
 
 func main() {
-	fmt.Println("Hello World!")
+	emptyChannel := make(chan bool)
+
+	// loading DOM to memory
+	dom = browser.LoadDOM()
+
+	// setting up everything
+	setup()
+
+	// attempt to receive from empty channel
+	// allow daemon style process
+	<-emptyChannel
+}
+
+func setup() {
+	// javascript rendering func
+	var renderer js.Func
+
+	renderer = js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		update()
+		return nil
+	})
+
+	// using browser animation frame
+	dom.Window.Call("requestAnimationFrame", renderer)
+
+	// let's handle that mouse pointer down
+	var mouseEventHandler = js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		clickEvent(args[0])
+		return nil
+	})
+
+	dom.Window.Call("addEventListener", "pointerdown", mouseEventHandler)
+}
+
+func update() {
+	fmt.Printf("updating frame")
+}
+
+func clickEvent(event js.Value) {
+	mouseX := event.Get("clientX").Int()
+	mouseY := event.Get("clientY").Int()
+
+	go dom.Log(fmt.Sprintf("mouseEvent x:%d y:%d", mouseX, mouseY))
 }
