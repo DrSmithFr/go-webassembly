26dc5c22f223652981b552c3266025d942518fdc
John Daligault <johndaligault@gmail.com>
2021-03-30 21:58:34 +0200

    Now using contex2D from go

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/DrSmithFr/go-webassembly
+
+go 1.16
+
+require (
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	github.com/llgcode/draw2d v0.0.0-20210313082411-577c1ead272a // indirect
+)
--- src/main.go
+++ src/main.go
@@ -2,10 +2,21 @@ package main
 
 import (
 	"fmt"
-	"github.com/DrSmithFr/go-webassembly/src/api"
 	"github.com/DrSmithFr/go-webassembly/src/browser"
+	"github.com/llgcode/draw2d/draw2dimg"
+	"github.com/llgcode/draw2d/draw2dkit"
+	"image/color"
 	"syscall/js"
 )
+
+var cvs *browser.Canvas2d
+
+var width float64
+var height float64
+
+var gs = gameState{laserSize: 35, directionX: 13.7, directionY: -13.7, laserX: 40, laserY: 40}
+
+type gameState struct{ laserX, laserY, directionX, directionY, laserSize float64 }
 
 func main() {
 	// loading DOM to memory
@@ -13,20 +24,24 @@ func main() {
 
 	// setting up everything
 	bindEvents(DOM)
-	bindAnimationFrame(DOM)
 
-	// Exposing our API
-	wrapper := api.GetJavascriptObject(DOM)
+	cvs, _ = browser.NewCanvas2d(false)
+	cvs.Create(
+		js.Global().Get("innerWidth").Int(),
+		js.Global().Get("innerHeight").Int(),
+	)
 
-	// give javascript control over the Go WASM API
-	callbackJavascript(DOM, wrapper)
+	height = float64(cvs.Height())
+	width = float64(cvs.Width())
+
+	cvs.Start(120, Render)
 
 	// allow daemon style process
 	emptyChanToKeepAppRunning := make(chan bool)
 	<-emptyChanToKeepAppRunning
 }
 
-func bindEvents(DOM browser.DOM)  {
+func bindEvents(DOM browser.DOM) {
 	// let's handle that mouse pointer down
 	var mouseEventHandler = js.FuncOf(func(this js.Value, args []js.Value) interface{} {
 		clickEvent(DOM, args[0])
@@ -36,37 +51,37 @@ func bindEvents(DOM browser.DOM)  {
 	DOM.Window.Call("addEventListener", "pointerdown", mouseEventHandler)
 }
 
-func bindAnimationFrame(DOM browser.DOM)  {
-	// javascript rendering func
-	var renderer js.Func
-
-	renderer = js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-		update(DOM)
-		DOM.Window.Call("requestAnimationFrame", renderer)
-		return nil
-	})
-
-	// using browser animation frame
-	DOM.Window.Call("requestAnimationFrame", renderer)
-}
-
-func callbackJavascript(DOM browser.DOM, ApiWrapper js.Value)  {
-	callback := DOM.Document.Get("onWasmLoad")
-
-	if callback.Type() == js.TypeFunction {
-		callback.Invoke(ApiWrapper)
-	} else {
-		panic("document.onWasmLoad() is undefined in current DOM")
-	}
-}
-
-func update(DOM browser.DOM) {
-	fmt.Printf("updating frame")
-}
-
 func clickEvent(DOM browser.DOM, event js.Value) {
 	mouseX := event.Get("clientX").Int()
 	mouseY := event.Get("clientY").Int()
 
 	go DOM.Log(fmt.Sprintf("mouseEvent x:%d y:%d", mouseX, mouseY))
+}
+
+func Render(gc *draw2dimg.GraphicContext) bool {
+
+	if gs.laserX+gs.directionX > width-gs.laserSize || gs.laserX+gs.directionX < gs.laserSize {
+		gs.directionX = -gs.directionX
+	}
+	if gs.laserY+gs.directionY > height-gs.laserSize || gs.laserY+gs.directionY < gs.laserSize {
+		gs.directionY = -gs.directionY
+	}
+
+	gc.SetFillColor(color.RGBA{0xff, 0xff, 0xff, 0xff})
+	gc.Clear()
+	// move red laser
+	gs.laserX += gs.directionX
+	gs.laserY += gs.directionY
+
+	// draws red 🔴 laser
+	gc.SetFillColor(color.RGBA{0xff, 0x00, 0xff, 0xff})
+	gc.SetStrokeColor(color.RGBA{0xff, 0x00, 0xff, 0xff})
+
+	gc.BeginPath()
+	// gc.ArcTo(gs.laserX, gs.laserY, gs.laserSize, gs.laserSize, 0, math.Pi*2)
+	draw2dkit.Circle(gc, gs.laserX, gs.laserY, gs.laserSize)
+	gc.FillStroke()
+	gc.Close()
+
+	return true
 }
